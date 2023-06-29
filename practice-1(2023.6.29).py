0be5582b7f1523ca6e5bceb8ed6db11e21956346
Park Sangmin <129809454+datecurator@users.noreply.github.com>
2023-06-29 16:16:25 +0900

    basic python study-1
    This is my python code study file.
    I will be studying continue!

---
+++ practice-1(2023.6.29).py
@@ -0,0 +1,72 @@
+# # 숫자 자료형
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# # 문자열 자료형
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+# print(9*"ㅋ")
+
+# # boolean 자료형
+# # 참 / 거짓
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not(5 > 10))
+
+# #  애완동물을 소개해 주세요!
+# print("우리집 강아지의 이름은 연탄이예요")
+# print("연탄이는 4살이며, 산책을 아주 좋아해요")
+# print("연탄이는 어른일까요? True")
+
+# animal = "강아지"
+# name = "연탄이"
+# age = 4
+# hobby = "산책"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "예요")
+# print(name + "는 " +str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+# animal = "고양이"
+# name = "해피"
+# age = 4
+# hobby = "낮잠"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "예요")
+# hobby = "공놀이"
+# # print(name + "는 " +str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name ,"는 ", age ,"살이며, ", hobby ,"을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+# # 주석처리
+# '''
+# 이렇게하면 
+# 문단 
+# 주석처리 
+# 가능
+# '''
+
+# #Quiz) 변수를 이용하여 다음 문장을 출력하시오
+# #변수명 : station
+# #변수값 : "사당", "심도림", "인천공항" 순서대로 입력
+# #출력문장 : XX행 열차가 들어오고 있습니다.
+
+# print("<답>")
+# station = "사당"
+# print(station + "행 열차가 들어오고 있습니다.")
+# station = "심도림"
+# print(station + "행 열차가 들어오고 있습니다.")
+# station = "인천공항"
+# print(station + "행 열차가 들어오고 있습니다.")
